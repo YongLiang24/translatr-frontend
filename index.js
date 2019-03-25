530ee41a86b093669db5a419c51d4e79fa329cc5
github username <github email address>
2019-03-25 14:26:29 -0700

    fetch

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const translateText = document.getElementById('translate-text')
 
 planningForm.addEventListener('submit', (ev)=>{
   ev.preventDefault();
-  console.log('click');
-  console.log(langSelect.value)
+  //console.log('click');
+  //console.log(langSelect.value)
   fetch('http://localhost:3000/api/v1/translate', {
     method: "POST",
     headers:{
